f338d0e40cd67a6abb3fe931fc76195e666476d3
taewonson <son593068@gmail.com>
2025-05-28 17:24:14 +0900

    자리선택 기능 구현

---
+++ WindowsFormsApp1/SeatStatusForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class SeatStatusForm : Form
+    {
+        private Seat seat;
+        public SeatStatusForm(Seat selectedSeat)
+        {
+            InitializeComponent();
+            seat = selectedSeat;
+            LoadSeatInfo();
+        }
+        private void LoadSeatInfo()
+        {
+            lblSeatId.Text = $"ID: {seat.SeatId}";
+            lblStatus.Text = $"상태: {seat.Status}";
+            lblPurpose.Text = $"설명: {seat.Purpose}";
+            lblReservedTime.Text = seat.Status == "예약 완료" ? $"예약 시간: {seat.ReservedTime}" : "";
+        }
+        private void btnReserve_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("예약창으로 이동합니다 (미구현)");
+            this.Close();
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("결제창으로 이동합니다 (미구현)");
+            this.Close();
+        }
+    }
+}
