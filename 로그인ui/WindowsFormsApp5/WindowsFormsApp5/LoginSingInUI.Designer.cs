4bde247d05071b01fcbd000d12f9834122def546
wkqeka <joinu2021@gmail.com>
2025-05-29 12:19:57 +0900

    회원가입 일부 기능 구현,ui

---
+++ 로그인ui/WindowsFormsApp5/WindowsFormsApp5/LoginSingInUI.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class LoginSingInUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phonenumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.Location = new System.Drawing.Point(231, 279);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(85, 29);
+            this.SignUpButton.TabIndex = 0;
+            this.SignUpButton.Text = "가입하기";
+            this.SignUpButton.UseVisualStyleBackColor = true;
+            this.SignUpButton.Click += new System.EventHandler(this.SignUpButton_Click);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(181, 74);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(125, 21);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(181, 116);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(125, 21);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(181, 159);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(83, 21);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // phonenumberTextBox
+            // 
+            this.phonenumberTextBox.Location = new System.Drawing.Point(181, 204);
+            this.phonenumberTextBox.Name = "phonenumberTextBox";
+            this.phonenumberTextBox.Size = new System.Drawing.Size(125, 21);
+            this.phonenumberTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 77);
+            this.label1.Name = "label1";    
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "비밀번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "이름";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "전화번호";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 375);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.phonenumberTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.SignUpButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SignUpButton;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phonenumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ 로그인ui/WindowsFormsApp5/WindowsFormsApp5/LoginUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class LoginUI : Form
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+        // 아래는 테스트 코드입니다.
+        // 함수안의 내용은 아무 의미 없으니 삭제후 사용하시면 됩니다.
+        #region 로그인 코드
+        private void onLoginClck(object sender, EventArgs e)
+        {
+            // 로그인 확인
+            bool test = true;
+            if (test == true) 
+            {
+                // 로그인 성공시 메인화면 진입
+                MessageBox.Show("로그인에 성공하셨습니다.");
+            }
+            else if(test == false)
+            {
+                // 로그인 실패시 실패 메시지 띄움
+                MessageBox.Show("로그인에 실패하였습니다.\n아이디나 비밀번호를 다시 확인해주세요.");
+            }
+        }
+        #endregion
+
+        #region 회원가입 코드
+        private void onNewAccountClick(object sender, EventArgs e)
+        {
+            // 회원가입 창 띄움
+        }
+        #endregion
+
+    }
+}
